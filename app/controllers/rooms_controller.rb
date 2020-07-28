29414d709def04834babfacef49136882e652c4b
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2020-07-27 21:18:15 -0500

    add the new function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,6 +6,26 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+  
+  def edit
+    @room = Room.find(params[:id]) 
+  end
+
+  def update
+    @room = Room.find(params[:id]) 
+    if @room.update(room_params)
+      redirect_to rooms_path, notice: "El producto ha sido modificado con éxito"
+    else
+      render :edit
+    end
+  end
+    
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+  
+    redirect_to rooms_path, notice: "El producto fue eliminado con éxito"
+  end
 
   def create
     @room = Room.new(room_params)
